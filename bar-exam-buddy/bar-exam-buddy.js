e9a3edc42e257f723b32cd0c734e762d495840bf
Franz Sarmiento <franzks@gmail.com>
2014-07-23 18:37:54 +0800

    Implemented hero transition for the exam-form

--- bar-exam-buddy/bar-exam-buddy.js
+++ bar-exam-buddy/bar-exam-buddy.js
@@ -1,5 +1,6 @@
 Polymer('bar-exam-buddy', {
     selectedPage: 0,
+    selectedHero: '',
     selectedPageChanged: function() {
         this.$.pages.selected = this.selectedPage;
     },
--- bar-exam-buddy/exam-card/exam-card.js
+++ bar-exam-buddy/exam-card/exam-card.js
@@ -1,6 +1,11 @@
 Polymer('exam-card', {
   examCardTitle: '',
+  ready: function() {
+    this.setAttribute('hero-id',this.examCardTitle);
+  },
   fabOnTap: function(e) {
     this.selectedPage = 1;
+    console.log(this.getAttribute('hero-id'));
+    this.selectedHero = this.getAttribute('hero-id');
   },
 });
