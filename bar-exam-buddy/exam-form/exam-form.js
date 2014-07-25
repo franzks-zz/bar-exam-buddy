cf6c6954cc80cf35551ecfba2f89a566bcb33dae
Franz Sarmiento <franzks@gmail.com>
2014-07-25 19:42:12 +0800

    Added async loading of dummy questions.

--- bar-exam-buddy/exam-form/exam-form.js
+++ bar-exam-buddy/exam-form/exam-form.js
@@ -1,6 +1,17 @@
 Polymer('exam-form', {
   questionCurrent: 0,
   questionTotal: 0,
+  ready: function() {
+    this.$.xhr.request({
+      url: 'dummy-questions.json',
+      callback: this.onQuestionsLoaded.bind(this)
+    });
+  },
+  arrQuestions: [],
+  onQuestionsLoaded: function(result) {
+    this.arrQuestions = eval(result);
+    this.questionTotal = this.arrQuestions.length;
+  },
   fabPrevOnTap: function(e) {
   },
   fabNextOnTap: function(e) {
