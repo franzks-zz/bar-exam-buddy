f090a3c25142cfd29df24c10c1b64e03dc8909ee
Franz Sarmiento <franzks@gmail.com>
2014-07-25 20:10:21 +0800

    Added dynamic loading of questions and choices

--- bar-exam-buddy/exam-form/exam-form.js
+++ bar-exam-buddy/exam-form/exam-form.js
@@ -9,9 +9,26 @@ Polymer('exam-form', {
   arrQuestions: [],
   onQuestionsLoaded: function(result) {
     this.arrQuestions = eval(result);
+    this.loadQuestion(0);
   },
-  fabPrevOnTap: function(e) {
+  loadQuestion: function(index) {
+    question = this.arrQuestions[index];
+    this.$.question.textContent = question.question;
+    this.$.choice1.label = question.choice1;
+    this.$.choice2.label = question.choice2;
+    this.$.choice3.label = question.choice3;
+    this.$.choice4.label = question.correctAnswer;
   },
-  fabNextOnTap: function(e) {
+  fabPrevOnTap: function() {
+    if(this.questionCurrent>0) {
+      this.questionCurrent--;
+      this.loadQuestion(this.questionCurrent);
+    }
+  },
+  fabNextOnTap: function() {
+    if(this.questionCurrent<this.arrQuestions.length-1) {
+      this.questionCurrent++;
+      this.loadQuestion(this.questionCurrent);
+    }
   },
 });
